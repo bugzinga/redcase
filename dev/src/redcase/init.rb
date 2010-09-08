36d5991755389afb87b26bd99ca13b0a2c598a78
Maxim Novikov <csbubbles@gmail.com>
2010-09-08 17:42:42 +0000

    1.0 alpha 0.128 Execution tab: test cases tree height is restricted (I'm back!)

--- dev/src/redcase/init.rb
+++ dev/src/redcase/init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :redcase do
     name        'Redcase'
     description 'Test cases management plugin for Redmine'
     author      'QA Project'
-    version     '1.0 alpha 0.127'
+    version     '1.0 alpha 0.128'
 
     permission  :view_test_cases,
                 :redcase => [

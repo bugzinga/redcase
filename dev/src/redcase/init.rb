90ca41f9e3509095de28578ac06d118266953de0
Maxim Novikov <csbubbles@gmail.com>
2010-11-23 12:51:23 +0000

    Bug fixed: state 'Obsoleted' renamed to 'Obsolete'

--- dev/src/redcase/init.rb
+++ dev/src/redcase/init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :redcase do
     name        'Redcase'
     description 'Test cases management plugin for Redmine'
     author      'QA Project'
-    version     '1.0 alpha 0.140'
+    version     '1.0 alpha 0.141'
 
     permission  :view_test_cases,
                 :redcase => [

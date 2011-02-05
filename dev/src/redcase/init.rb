21ce8a366304092362868ee3d2632580d32fd5fa
Maxim Novikov <csbubbles@gmail.com>
2011-02-05 09:01:43 +0000

    migrate script #15 corrected (backward rename of order_num to order in self.down)

--- dev/src/redcase/init.rb
+++ dev/src/redcase/init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :redcase do
     name        'Redcase'
     description 'Test cases management plugin for Redmine'
     author      'QA Project'
-    version     '1.0 alpha 0.142'
+    version     '1.0 alpha 0.143'
 
     permission  :view_test_cases,
                 :redcase => [

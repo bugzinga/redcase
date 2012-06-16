31e272002cc3f7c6ae60b72be7257d0fe7f680ea
Maxim Novikov <csbubbles@gmail.com>
2012-06-16 23:53:55 +0000

    fix for redmine 0.9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 
-# Plugin's routes
-# See: http://guides.rubyonrails.org/routing.html
-get 'index', :to => 'redcase#index'
+if ( Rails::version().split('.').first.to_i > 2)
+    get 'index', :to => 'redcase#index'
+end
--- init.rb
+++ init.rb
@@ -2,22 +2,24 @@
 require 'redmine'
 
 Redmine::Plugin.register :redcase do
+
     name 'Redcase'
     description 'Test case management plugin for Redmine'
-    version '1.0-alpha-2.19'
+    version '1.0-alpha-2.20'
     url 'http://redcase.sourceforge.net'
     author 'Redcase Dev Team'
-    
+
     permission :redcase, { :redcase => [:index] }, :public => true
     menu    :project_menu,
             :redcase,
-                {
-                    :controller => 'redcase',
-                    :action     => 'index'
-                },
-                {
-                    :param   => :project_id,
-                    :caption => :redcase_tab_title,
-                    :after   => :new_issue
-                }  
+            {
+                :controller => 'redcase',
+                :action     => 'index'
+            },
+            {
+                :param   => :project_id,
+                :caption => :redcase_tab_title,
+                :after   => :new_issue
+            }
+
 end

eeaa4c533b970d2f007fefc74bfbf9d86b364778
Maxim Novikov <csbubbles@gmail.com>
2012-06-17 12:54:44 +0000

    tests fixed for rails 2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 
 # Load the normal Rails helper
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+if Redcase::System::rails2?
+    test_helper = File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
+elsif Redcase::System::rails3?
+    test_helper = File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+end
+
+require test_helper
 
 # Ensure that we are using the temporary fixture path
 #Engines::Testing.set_fixture_path

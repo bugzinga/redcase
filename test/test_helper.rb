9ffe9c342efa76dd9f2a169e0d9ebaa51ba77fe0
Maxim Novikov <csbubbles@gmail.com>
2012-06-18 03:40:37 +0000

    minor changes: housekeeping

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,18 @@
 
-current_folder = File.dirname(__FILE__)
-
 # Workaround to build the path to 'test_helper' for different Redmine versions
+current_folder = File.dirname(__FILE__)
+test_helper_path = current_folder
 if current_folder.include? 'vendor'
-    test_helper_relative_path = '/../../../../test/test_helper'
-else
-    test_helper_relative_path = '/../../../test/test_helper'
+    test_helper_path += '/..'
 end
+test_helper_path += '/../../../test/test_helper'
 
-require File.expand_path(current_folder + test_helper_relative_path)
+require File.expand_path(test_helper_path)
 
 class Test::Unit::TestCase
+
     include Redmine::I18n
-    
+
     def log
         @log ||= Logger.new(STDOUT)
     end

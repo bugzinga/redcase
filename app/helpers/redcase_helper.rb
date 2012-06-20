89f28d9183d79f19ea157895bbf797b44cc454f2
Dmitry Rybakov <dmitry.rybakov@gmail.com>
2012-06-20 03:05:51 +0000

    Feature #1

--- app/helpers/redcase_helper.rb
+++ app/helpers/redcase_helper.rb
@@ -3,12 +3,21 @@ module RedcaseHelper
 
     include ApplicationHelper
 
-    def get_redcase_inner_tabs
-        [
-            { :name => 'Management', :partial => 'redcase/management', :label => :redcase_i18n_tab_management },
-            { :name => 'Execution', :partial => 'redcase/execution', :label => :redcase_i18n_tab_execution },
-            { :name => 'Report', :partial => 'redcase/report', :label => :redcase_i18n_tab_report }
-        ]
+    def get_redcase_inner_tabs(project)
+        tabs = []
+        if User.current.allowed_to?(:redcase_management, project)
+            tabs << { :name => 'Management', :partial => 'redcase/management', :label => :redcase_i18n_tab_management } 
+        end
+        
+        if User.current.allowed_to?(:redcase_execution, project)
+            tabs << { :name => 'Execution', :partial => 'redcase/execution', :label => :redcase_i18n_tab_execution } 
+        end
+        
+        if User.current.allowed_to?(:redcase_report, project)
+            tabs << { :name => 'Report', :partial => 'redcase/report', :label => :redcase_i18n_tab_report } 
+        end
+        
+        return tabs
     end
 
 end
--- init.rb
+++ init.rb
@@ -6,11 +6,13 @@ Redmine::Plugin.register :redcase do
 
     name 'Redcase'
     description 'Test case management plugin for Redmine'
-    version '1.0-alpha-2.35'
+    version '1.0-alpha-2.36'
     url 'http://redcase.sourceforge.net'
     author 'Redcase Dev Team'
 
-    permission :redcase, { :redcase => [ :index ] }, :public => true
+    permission :redcase_management, :redcase => :index, :require => :member
+    permission :redcase_execution, :redcase => :index, :require => :member
+    permission :redcase_report, :redcase => :index, :require => :member
 
     menu :project_menu,
          :redcase,

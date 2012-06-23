0c734b32e7ff7e90cbaa830b7e26abc9f45a8ac4
Maxim Novikov <csbubbles@gmail.com>
2012-06-23 03:05:08 +0000

    housekeeping

--- app/helpers/redcase_helper.rb
+++ app/helpers/redcase_helper.rb
@@ -5,13 +5,13 @@ module RedcaseHelper
 
     def get_redcase_inner_tabs(project)
         tabs = []
-        if User.current.allowed_to?(::I18n.t(:redcase_i18n_permission_manage), project)
+        if User.current.allowed_to?(:redcase_i18n_permission_manage, project)
             tabs << { :name => 'Management', :partial => 'redcase/management', :label => :redcase_i18n_tab_management }
         end
-        if User.current.allowed_to?(::I18n.t(:redcase_i18n_permission_execute), project)
+        if User.current.allowed_to?(:redcase_i18n_permission_execute, project)
             tabs << { :name => 'Execution', :partial => 'redcase/execution', :label => :redcase_i18n_tab_execution }
         end
-        if User.current.allowed_to?(::I18n.t(:redcase_i18n_permission_view), project)
+        if User.current.allowed_to?(:redcase_i18n_permission_view, project)
             tabs << { :name => 'Report', :partial => 'redcase/report', :label => :redcase_i18n_tab_report }
         end
         tabs
--- init.rb
+++ init.rb
@@ -2,19 +2,17 @@
 require 'redmine'
 require 'redcase'
 
-I18n.load_path << Dir[File.dirname(__FILE__) + "/config/locales/*.{rb,yml}"]
-
 Redmine::Plugin.register :redcase do
 
     name 'Redcase'
     description 'Test case management plugin for Redmine'
-    version '1.0-alpha-2.38'
+    version '1.0-alpha-2.39'
     url 'http://redcase.sourceforge.net'
     author 'Redcase Dev Team'
 
-    permission ::I18n.t(:redcase_i18n_permission_manage), :redcase => :index, :require => :member
-    permission ::I18n.t(:redcase_i18n_permission_execute), :redcase => :index, :require => :member
-    permission ::I18n.t(:redcase_i18n_permission_view), :redcase => :index, :require => :member
+    permission :redcase_i18n_permission_manage, :redcase => :index, :require => :member
+    permission :redcase_i18n_permission_execute, :redcase => :index, :require => :member
+    permission :redcase_i18n_permission_view, :redcase => :index, :require => :member
 
     menu :project_menu,
          :redcase,
@@ -24,7 +22,7 @@ Redmine::Plugin.register :redcase do
          },
          {
              :param   => :project_id,
-             :caption => ::I18n.t(:redcase_i18n_tab),
+             :caption => :redcase_i18n_tab,
              :after   => :new_issue
          }
 

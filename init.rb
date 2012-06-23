8f0ab2f971c514e69b7af6d5a8504d979359f06d
Dmitry Rybakov <dmitry.rybakov@gmail.com>
2012-06-23 23:28:23 +0000

    Feature #1

--- init.rb
+++ init.rb
@@ -6,13 +6,15 @@ Redmine::Plugin.register :redcase do
 
     name 'Redcase'
     description 'Test case management plugin for Redmine'
-    version '1.0-alpha-2.42'
+    version '1.0-alpha-2.43'
     url 'http://redcase.sourceforge.net'
     author 'Redcase Dev Team'
+
 
     permission :redcase_manage_test_cases, :redcase => :index, :require => :member
     permission :redcase_execute_test_cases, :redcase => :index, :require => :member
     permission :redcase_view_test_results, :redcase => :index, :require => :member
+
 
     menu :project_menu,
          :redcase,
@@ -22,6 +24,15 @@ Redmine::Plugin.register :redcase do
          },
          {
              :param   => :project_id,
+             :if => lambda { |p|
+                 (
+                    User.current.allowed_to?(:redcase_manage_test_cases, p) or 
+                    User.current.allowed_to?(:redcase_manage_test_cases, p) or
+                    User.current.allowed_to?(:redcase_manage_test_cases, p)
+                ) and (
+                    p.trackers.select { |t| t.name == ::I18n.t(:redcase_i18n_tracker) }.length > 0
+                )
+             },
              :caption => :redcase_i18n_tab,
              :after   => :new_issue
          }

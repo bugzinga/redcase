3d60ccd3c0c75a3c2388480edac34d7f97817d3a
Maxim Novikov <csbubbles@gmail.com>
2012-06-23 07:01:10 +0000

    test fixture fixed

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redcase do
 
     name 'Redcase'
     description 'Test case management plugin for Redmine'
-    version '1.0-alpha-2.40'
+    version '1.0-alpha-2.41'
     url 'http://redcase.sourceforge.net'
     author 'Redcase Dev Team'
 

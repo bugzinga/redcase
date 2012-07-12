a1921c9495331366855a9013c06dd20eabc3f726
Dmitry Rybakov <dmitry.rybakov@gmail.com>
2012-07-12 12:27:59 +0000

    It should run under 1.4.x

---
+++ app/helpers/redcase_test_suites_helper.rb
@@ -0,0 +1,5 @@
+module RedcaseTestSuitesHelper
+    def tr_id(node)
+        "#{node.class}:#{node.id}"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ if Redcase::System::rails3?
     get 'redcase', :to => 'redcase#index'
     get 'redcase_context_menus/test_suites_tree'
     resources :redcase_test_suites
+else
+    ActionController::Routing::Routes.draw do |map|
+        map.connect '/redcase', :controller => 'redcase', :action => 'index'
+        map.connect 'redcase_context_menus/test_suites_tree', :controller => 'redcase_context_menus', :action => 'test_suites_tree'
+        map.resources :redcase_test_suites
+    end
 end

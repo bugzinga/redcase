017e0cd53fbd371732da11d7f6d7f1d798e89664
Maxim Novikov <csbubbles@gmail.com>
2014-02-01 07:42:39 -0800

    Minor change: tabs to spaces in helper

--- app/helpers/redcase_helper.rb
+++ app/helpers/redcase_helper.rb
@@ -9,18 +9,18 @@ module RedcaseHelper
   include ApplicationHelper
 
   class RedcasePerformance < ActionController::Base
-		def start(action)
-			@action = action
-			@start_time = Time.now
-			logger.info "[Redcase] Time calculation: #{@action}"
-		end
-		def stop()
-			result = (Time.now - @start_time)
-			logger.info "#{result.round(4)} sec(s): #{@action}"
-		end
-	end
+    def start(action)
+      @action = action
+      @start_time = Time.now
+      logger.info "[Redcase] Time calculation: #{@action}"
+    end
+    def stop()
+      result = (Time.now - @start_time)
+      logger.info "#{result.round(4)} sec(s): #{@action}"
+    end
+  end
 
-	def test_cases_count(test_suite)
+  def test_cases_count(test_suite)
     test_suite.test_cases.count + test_suite.children.inject(0) { |res, x| res + test_cases_count(x) }
   end
 
@@ -28,15 +28,15 @@ module RedcaseHelper
   # test suites (for 'obsolete' and 'unsorted' test cases) if they are not exist yet
   def test_suite_root(project)
     test_suite = TestSuite.find_by_project_id(
-			project.id,
-			:joins => :children,
-			:include => [
-				{ :children => [
+      project.id,
+      :joins => :children,
+      :include => [
+        { :children => [
             { :children => { :test_cases => [ { :issue => [ :author, :status, :priority ] } ] }} ,
             { :test_cases => [ { :issue => [ :author, :status, :priority ] } ] } ] },
-				[ { :children => :children }, { :test_cases => [{ :issue => [:author, :status, :priority] } ] } ]
-			])
-		#test_suite = test_suites.detect { |x| x.project_id == project.id }
+        [ { :children => :children }, { :test_cases => [{ :issue => [:author, :status, :priority] } ] } ]
+      ])
+    #test_suite = test_suites.detect { |x| x.project_id == project.id }
     if test_suite.nil? then
       test_suite = TestSuite.create(:name => "Root");
       test_suite.project = project
@@ -162,7 +162,7 @@ module RedcaseHelper
 
   def test_case_to_json(test_case)
     textilized_description = (textilizable(test_case.issue.description.gsub(/#\d/) { |s| s.gsub("#", "N") }) if test_case.issue.description)
-		{
+    {
       'issue_id'     => test_case.issue_id,
       'text'         => test_case.issue.subject,
       'editable'     => false,
@@ -175,10 +175,10 @@ module RedcaseHelper
         :width => '500',
         :closable => 'true',
         :text => '"' + test_case.issue.subject + '"<br/>' +
-					(test_case.issue.description.nil? ? '' : ('<br/><b>Description:</b><br/>' + textilized_description)) +
-					'<br/><b>Priority:</b> ' + test_case.issue.priority.name +
-					'<br/><b>Author:</b> ' + test_case.issue.author.name +
-					'<br/><b>Created:</b> ' + test_case.issue.created_on.strftime('%d.%m.%Y %H:%M'),
+          (test_case.issue.description.nil? ? '' : ('<br/><b>Description:</b><br/>' + textilized_description)) +
+          '<br/><b>Priority:</b> ' + test_case.issue.priority.name +
+          '<br/><b>Author:</b> ' + test_case.issue.author.name +
+          '<br/><b>Created:</b> ' + test_case.issue.created_on.strftime('%d.%m.%Y %H:%M'),
         :title => ('Issue #' + test_case.issue.id.to_s),
         :dismissDelay => 30000
       },
@@ -204,11 +204,11 @@ module RedcaseHelper
 
   def test_suite_to_json(suite)
     if suite.parent_id then
-			#logger.info("Child: #{suite.name}");
+      #logger.info("Child: #{suite.name}");
       kids = suite.children.collect { |s|
-				#logger.info("select not root #{s.name}");
-				test_suite_to_json(s)
-			} + suite.test_cases.sort_by { |x|
+        #logger.info("select not root #{s.name}");
+        test_suite_to_json(s)
+      } + suite.test_cases.sort_by { |x|
         #logger.info("sort_by not root #{x.issue.subject}");
         x.issue.subject
       }.collect { |tc|
@@ -216,28 +216,28 @@ module RedcaseHelper
         test_case_to_json(tc)
       }
     else
-			#logger.info("Root: #{suite.name}");
+      #logger.info("Root: #{suite.name}");
       kids = suite.children.select { |x|
-				#logger.info("select not obsolete/unsorted #{x.name}");
-				(x.name != '.Obsolete' and x.name != '.Unsorted')
-			}.collect { |s|
-				#logger.info("select not/collect");
-				test_suite_to_json(s)
-			} +
-				suite.test_cases.sort_by { |x|
+        #logger.info("select not obsolete/unsorted #{x.name}");
+        (x.name != '.Obsolete' and x.name != '.Unsorted')
+      }.collect { |s|
+        #logger.info("select not/collect");
+        test_suite_to_json(s)
+      } +
+        suite.test_cases.sort_by { |x|
         #logger.info("sort_by");
         x.issue.subject }.collect { |tc|
         #logger.info("sort_by/collect");
         test_case_to_json(tc)
       } +
-				suite.children.select { |x|
+        suite.children.select { |x|
         #logger.info("select obsolete/unsorted #{x.name}");
         (x.name == '.Obsolete' or x.name == '.Unsorted') }.collect { |s|
         #logger.info("select/collect");
         test_suite_to_json(s)
       }
     end
-		{
+    {
       'suite_id'       => suite.id,
       'text'           => suite.name,
       'id'             => suite.id,
@@ -251,25 +251,25 @@ module RedcaseHelper
       'counts'         => 0 #get_test_suite_counts_tree(find_test_suite_root(self)).to_json
     }
 		
-	end
+  end
 
   def get_url_for(action, project_id)
     "#{url_for(:action => action, :project_id => project_id)}"
   end
 	
-	def get_execution_suites_by_project_and_parent(project, parent = nil)
-		ExecutionSuite.find_all_by_project_id_and_parent_id(project.id, parent.nil? ? nil : parent.id)
-	end
+  def get_execution_suites_by_project_and_parent(project, parent = nil)
+    ExecutionSuite.find_all_by_project_id_and_parent_id(project.id, parent.nil? ? nil : parent.id)
+  end
 
-	def get_environments_by_project(project)
-		ExecutionEnvironment.find_all_by_project_id(project.id)
-	end
+  def get_environments_by_project(project)
+    ExecutionEnvironment.find_all_by_project_id(project.id)
+  end
 
-	def update_execution_suite_link(icon, act, size = '20x20')
+  def update_execution_suite_link(icon, act, size = '20x20')
     link_to_remote image_tag(icon, :plugin => 'redcase', :size=> size, :style => 'vertical-align: middle'), :url => { :action => 'update_exelists', :project_id => @project.id, :act => act }, :update => :management_execution_suites_id, :submit => 'list_form', :complete => 'full()'
   end
 
-	def update_environment_link(icon, act, size = '20x20')
+  def update_environment_link(icon, act, size = '20x20')
     link_to_remote image_tag(icon, :plugin => 'redcase', :size=> size, :style => 'vertical-align: middle'), :url => { :action => 'update_environment', :project_id => @project.id, :act => act }, :update => :management_environments_id, :submit => 'environment_form_id', :complete => 'full()'
   end
 

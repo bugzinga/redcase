40c7f6268a1d6d0f85875a36eddb44a4f1e35c42
Maxim Novikov <csbubbles@gmail.com>
2014-02-01 19:44:37 -0800

    Fixed test case dragging to execution suite

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+
 match 'projects/:id/redcase', :to => 'redcase#index', :via => 'get'
 match 'projects/:id/redcase/index', :to => 'redcase#index', :via => 'get'
 match 'projects/:id/redcase/execution_suite_manager', :to => 'redcase#execution_suite_manager'
-match 'redcase/execution_suite_manager', :to => 'redcase#execution_suite_manager'
+match 'projects/:id/redcase/execution_suite_manager', :to => 'redcase#execution_suite_manager'
+match 'projects/:id/redcase/copy_test_case_to_exec', :to => 'redcase#copy_test_case_to_exec'
+
 match ':controller(/:action(/:id))(.:format)'

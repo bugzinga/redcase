c39162bc3479eb80a4b29e28990f7d7a98f352f8
Maxim Novikov <csbubbles@gmail.com>
2014-02-01 20:25:38 -0800

    Test suites creation fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 
-match 'projects/:id/redcase', :to => 'redcase#index', :via => 'get'
-match 'projects/:id/redcase/index', :to => 'redcase#index', :via => 'get'
+match 'projects/:id/redcase', :to => 'redcase#index'
+match 'projects/:id/redcase/index', :to => 'redcase#index'
+match 'projects/:id/redcase/test_suite_manager', :to => 'redcase#test_suite_manager'
 match 'projects/:id/redcase/execution_suite_manager', :to => 'redcase#execution_suite_manager'
 match 'projects/:id/redcase/copy_test_case_to_exec', :to => 'redcase#copy_test_case_to_exec'
-match 'projects/:id/redcase/get_test_case', :to => 'redcase#get_test_case', :via => 'get'
-match 'projects/:id/redcase/get_executions', :to => 'redcase#get_executions', :via => 'get'
-match 'projects/:id/redcase/get_attachment_urls', :to => 'redcase#get_attachment_urls', :via => 'get'
+match 'projects/:id/redcase/get_test_case', :to => 'redcase#get_test_case'
+match 'projects/:id/redcase/get_executions', :to => 'redcase#get_executions'
+match 'projects/:id/redcase/get_attachment_urls', :to => 'redcase#get_attachment_urls'
 match 'projects/:id/redcase/execute', :to => 'redcase#execute'
 
 match ':controller(/:action(/:id))(.:format)'

d0cf62845e1caef49f96f262e4eb9f152d10a0f4
Maxim Novikov <csbubbles@gmail.com>
2014-02-01 19:36:46 -0800

    Bug #1 Plugin fails with "uninitialized constant RedcaseDefaultTracker" error

--- db/migrate/013_redcase_default_tracker.rb
+++ db/migrate/013_redcase_default_tracker.rb
@@ -2,19 +2,19 @@
 class RedcaseDefaultTracker < ActiveRecord::Migration
 
   def self.up
-    tc = Tracker.find_by_name('Test case')
-    tc = Tracker.create(:name => 'Test case') if not tc
-    new_st = IssueStatus.find_by_name('New')
-    new_st = IssueStatus.create(:name => "New", :is_closed => false, :is_default => false) if not new_st
-    active_st = IssueStatus.find_by_name('In Progress')
-    active_st = IssueStatus.create(:name => "In Progress", :is_closed => false, :is_default => false) if not active_st
-    obsoleted_st = IssueStatus.find_by_name('Obsoleted')
-    obsoleted_st = IssueStatus.create(:name => "Obsoleted", :is_closed => false, :is_default => false) if not obsoleted_st
-    Role.find(:all).each{ |rl|
-      WorkflowRule.create!(:tracker_id => tc.id, :role_id => rl.id , :old_status_id => new_st.id, :new_status_id => active_st.id)
-      WorkflowRule.create!(:tracker_id => tc.id, :role_id => rl.id , :old_status_id => active_st.id, :new_status_id => new_st.id)
-      WorkflowRule.create!(:tracker_id => tc.id, :role_id => rl.id , :old_status_id => active_st.id, :new_status_id => obsoleted_st.id)
-      WorkflowRule.create!(:tracker_id => tc.id, :role_id => rl.id , :old_status_id => obsoleted_st.id, :new_status_id => new_st.id)
+    tracker = Tracker.find_by_name('Test case')
+    tracker = Tracker.create(:name => 'Test case') if not tracker
+    new_status = IssueStatus.find_by_name('New')
+    new_status = IssueStatus.create(:name => "New", :is_closed => false, :is_default => false) if not new_status
+    active_status = IssueStatus.find_by_name('In Progress')
+    active_status = IssueStatus.create(:name => "In Progress", :is_closed => false, :is_default => false) if not active_status
+    obsoleted_status = IssueStatus.find_by_name('Obsoleted')
+    obsoleted_status = IssueStatus.create(:name => "Obsoleted", :is_closed => false, :is_default => false) if not obsoleted_status
+    Role.all.each { |role|
+      WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => new_status, :new_status => active_status)
+      WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => active_status, :new_status => new_status)
+      WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => active_status, :new_status => obsoleted_status)
+      WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => obsoleted_status, :new_status => new_status)
     }
   end
 

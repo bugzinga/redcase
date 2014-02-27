8fefd28494d1210d06544dc5f38a6177d9426ce9
Maxim Novikov <csbubbles@gmail.com>
2014-02-27 13:16:43 -0800

    Bug #23 Error during uninstall

--- db/migrate/011_redcase_project_es.rb
+++ db/migrate/011_redcase_project_es.rb
@@ -11,7 +11,6 @@ class RedcaseProjectEs < ActiveRecord::Migration
       t.integer :execution_suite_id, :null => false
       t.integer :project_id, :null => false
     end
-    add_index :execution_suite_project, [:project_id], :unique => true
     add_index :execution_suite_project, [:execution_suite_id, :project_id], :unique => true, :name => "execution_suite_project_index"
     remove_column :execution_suites, :project_id
   end

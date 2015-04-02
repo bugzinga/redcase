7f3128235b40e2a007a868a192753be52c13b0b3
David Robinson <drobinson@consit.dk>
2015-04-02 21:39:03 +0200

    Remove legacy "is_default" field from migration scripts
    * Migration adding IssueStatuses changed to reflect the fact that there is no
      longer an "is_default" attribute on IssueStatuses.

--- db/migrate/013_redcase_default_tracker.rb
+++ db/migrate/013_redcase_default_tracker.rb
@@ -5,11 +5,11 @@ class RedcaseDefaultTracker < ActiveRecord::Migration
     tracker = Tracker.find_by_name('Test case')
     tracker = Tracker.create(:name => 'Test case') if not tracker
     new_status = IssueStatus.find_by_name('New')
-    new_status = IssueStatus.create(:name => "New", :is_closed => false, :is_default => false) if not new_status
+    new_status = IssueStatus.create(:name => "New", :is_closed => false) if not new_status
     active_status = IssueStatus.find_by_name('In Progress')
-    active_status = IssueStatus.create(:name => "In Progress", :is_closed => false, :is_default => false) if not active_status
+    active_status = IssueStatus.create(:name => "In Progress", :is_closed => false) if not active_status
     obsoleted_status = IssueStatus.find_by_name('Obsoleted')
-    obsoleted_status = IssueStatus.create(:name => "Obsoleted", :is_closed => false, :is_default => false) if not obsoleted_status
+    obsoleted_status = IssueStatus.create(:name => "Obsoleted", :is_closed => false) if not obsoleted_status
     Role.all.each { |role|
       WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => new_status, :new_status => active_status)
       WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => active_status, :new_status => new_status)

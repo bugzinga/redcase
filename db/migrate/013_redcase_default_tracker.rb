7c8d4bd86f6dd4c44aa53559247457ee08eb3ce3
Dmitry Rybakov <dmitry.rybakov@gmail.com>
2015-04-03 11:49:37 +0200

    Bug #51 Cannot run plugin migration on fresh Redmine

--- db/migrate/013_redcase_default_tracker.rb
+++ db/migrate/013_redcase_default_tracker.rb
@@ -2,14 +2,22 @@
 class RedcaseDefaultTracker < ActiveRecord::Migration
 
   def self.up
-    tracker = Tracker.find_by_name('Test case')
-    tracker = Tracker.create(:name => 'Test case') if not tracker
-    new_status = IssueStatus.find_by_name('New')
-    new_status = IssueStatus.create(:name => "New", :is_closed => false) if not new_status
-    active_status = IssueStatus.find_by_name('In Progress')
-    active_status = IssueStatus.create(:name => "In Progress", :is_closed => false) if not active_status
-    obsoleted_status = IssueStatus.find_by_name('Obsoleted')
-    obsoleted_status = IssueStatus.create(:name => "Obsoleted", :is_closed => false) if not obsoleted_status
+    new_status = IssueStatus.where(
+      :name => "New",
+      :is_closed => false
+    ).first_or_create
+    active_status = IssueStatus.where(
+      :name => "In Progress",
+      :is_closed => false
+    ).first_or_create
+    obsoleted_status = IssueStatus.where(
+      :name => "Obsoleted",
+      :is_closed => false
+    ).first_or_create
+    tracker = Tracker.where(
+      :name => 'Test case',
+      :default_status => new_status
+    ).first_or_create
     Role.all.each { |role|
       WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => new_status, :new_status => active_status)
       WorkflowTransition.create!(:tracker => tracker, :role => role , :old_status => active_status, :new_status => new_status)

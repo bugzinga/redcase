9134ef4e2153f5f51b9d9edb37cad7a5d40160db
Maxim Novikov <csbubbles@gmail.com>
2015-06-01 21:06:36 -0700

    Clean up init script and license file

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+
 require 'redmine'
 require 'issue_patch'
 require 'project_patch'
@@ -6,116 +7,113 @@ require 'user_patch'
 require 'redcase_override'
 
 Redmine::Plugin.register :redcase do
-	
+
 	name 'Redcase'
 	description 'Test cases management plugin for Redmine'
 	author 'Evgeny Anikiev, Maxim Novikov & David Robinson'
-	version '0.2'
+	version '1.0-RC-1'
 
-	permission :view_test_cases,
-		{
-			:redcase => [
-				:index,
-				:get_attachment_urls
-			],
-			'redcase/environments' => [
-				:index
-			],
-			'redcase/testsuites' => [
-				:index
-			],
-			'redcase/testcases' => [
-				:index
-			],
-			'redcase/executionjournals' => [
-				:index
-			],
-			'redcase/export' => [
-				:index
-			],			
-			'redcase/executionsuites' => [
-				:index,
-				:show
-			],			
-			'redcase/graph' => [
-				:show
-			],
-			'redcase/combos' => [
-				:index
-			]			
-		}
+	permission :view_test_cases, {
+		:redcase => [
+			:index,
+			:get_attachment_urls
+		],
+		'redcase/environments' => [
+			:index
+		],
+		'redcase/testsuites' => [
+			:index
+		],
+		'redcase/testcases' => [
+			:index
+		],
+		'redcase/executionjournals' => [
+			:index
+		],
+		'redcase/export' => [
+			:index
+		],
+		'redcase/executionsuites' => [
+			:index,
+			:show
+		],
+		'redcase/graph' => [
+			:show
+		],
+		'redcase/combos' => [
+			:index
+		]
+	}
 
-	permission :edit_test_cases,
-		{
-			:redcase => [
-				:index,
-				:get_attachment_urls
-			],
-			'redcase/environments' => [
-				:index,
-				:update,
-				:destroy,
-				:create
-			],
-			'redcase/testsuites' => [
-				:index,
-				:update,
-				:destroy,
-				:create
-			],
-			'redcase/testcases' => [
-				:index,
-				:update,
-				:destroy,
-				:copy
-			],
-			'redcase/executionjournals' => [
-				:index
-			],
-			'redcase/export' => [
-				:index
-			],			
-			'redcase/executionsuites' => [
-				:index,
-				:update,
-				:destroy,
-				:create,
-				:show
-			],			
-			'redcase/graph' => [
-				:show
-			],
-			'redcase/combos' => [
-				:index
-			]			
-		}
+	permission :edit_test_cases, {
+		:redcase => [
+			:index,
+			:get_attachment_urls
+		],
+		'redcase/environments' => [
+			:index,
+			:update,
+			:destroy,
+			:create
+		],
+		'redcase/testsuites' => [
+			:index,
+			:update,
+			:destroy,
+			:create
+		],
+		'redcase/testcases' => [
+			:index,
+			:update,
+			:destroy,
+			:copy
+		],
+		'redcase/executionjournals' => [
+			:index
+		],
+		'redcase/export' => [
+			:index
+		],
+		'redcase/executionsuites' => [
+			:index,
+			:update,
+			:destroy,
+			:create,
+			:show
+		],
+		'redcase/graph' => [
+			:show
+		],
+		'redcase/combos' => [
+			:index
+		]
+	}
 
-	permission :execute_test_cases,
-		{
-			:redcase => [
-				:index,
-				:get_attachment_urls
-			],
-			'redcase/environments' => [
-				:index
-			],
-			'redcase/testsuites' => [
-				:index
-			],
-			'redcase/testcases' => [
-				:index,
-				:update
-			],
-			'redcase/executionjournals' => [
-				:index
-			],
-			'redcase/export' => [
-				:index
-			],			
-			'redcase/executionsuites' => [
-				:index
-			]		
-		}
+	permission :execute_test_cases, {
+		:redcase => [
+			:index,
+			:get_attachment_urls
+		],
+		'redcase/environments' => [
+			:index
+		],
+		'redcase/testsuites' => [
+			:index
+		],
+		'redcase/testcases' => [
+			:index,
+			:update
+		],
+		'redcase/executionjournals' => [
+			:index
+		],
+		'redcase/export' => [
+			:index
+		],
+		'redcase/executionsuites' => [
+			:index
+		]
+	}
 
 	menu :project_menu,
 		:redcase, {
@@ -123,7 +121,10 @@ Redmine::Plugin.register :redcase do
 			:action => 'index'
 		}, {
 			:if => proc { |p|
-				(User.current.allowed_to?(:view_test_cases, p) or User.current.allowed_to?(:edit_test_cases, p)) and (p.trackers.select { |t| t.name == 'Test case' }.length > 0)
+				can_view = User.current.allowed_to?(:view_test_cases, p)
+				can_edit = User.current.allowed_to?(:edit_test_cases, p)
+				tracker_exists = p.trackers.any? { |t| (t.name == 'Test case') }
+				(can_view || can_edit) && tracker_exists
 			},
 			:caption => 'Test cases',
 			:after => :new_issue
@@ -135,5 +136,6 @@ Redmine::Plugin.register :redcase do
 		Version.send :include, VersionPatch
 		User.send :include, UserPatch
 	end
-	
+
 end
+
